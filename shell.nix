6b84f9b928d8f4f2a67a09fd5191cb3c798677b8
Talafumi Asano <cariandrum22@gmail.com>
2021-02-04 11:37:31 +0900

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "a5fd0328827ac46954db08f624c09eba981f1ab2";
+      sha256 = "1g3bk2y8hz0y998yixz3jmvh553kjpj2k7j0xrp4al1jrbdcmgjq";
+    }) {
+    inherit pkgs;
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    easy-ps.purs-0_13_8
+    easy-ps.psc-package
+    easy-ps.spago
+    easy-ps.purty
+    easy-ps.zephyr
+    pkgs.nodejs
+    pkgs.dhall-lsp-server
+  ];
+}
